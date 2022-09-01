42e743060c9113d8abb2d253e7405fbddd62cbb3
Aradhana Indra D(ev) <aradhanaindradev@gmail.com>
2022-09-01 20:05:39 +0700

    Added responsiveness up to small laptop

---
+++ script.js
@@ -0,0 +1,28 @@
+$(function() {
+  $('#Nindya').on("click", function(){
+    // Selector
+    $(this).addClass('selected');
+    $('#Mentor, #Mentee').removeClass('selected');
+
+    // Content
+    $('.nindya').css("display", "flex");
+    $('.mentor, .mentee').css("display", "none");
+  })
+  $('#Mentor').on("click", function(){
+    // Selector
+    $(this).addClass('selected');
+    $('#Nindya, #Mentee').removeClass('selected');
+    
+    // Content
+    $('.mentor').css("display", "flex");
+    $('.nindya, .mentee').css("display", "none");
+  })
+  $('#Mentee').on("click", function(){
+    // Selector
+    $(this).addClass('selected');
+    $('#Nindya, #Mentor').removeClass('selected');
+
+    $('.mentee').css("display", "flex");
+    $('.nindya, .mentor').css("display", "none");
+  })
+});
